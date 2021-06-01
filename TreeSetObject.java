94c1efc89424011eb04effc0f99afe7ef0e43c6d
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-06-01 21:33:23 +0530

    TreeSet

---
+++ TreeSetObject.java
@@ -0,0 +1,21 @@
+public class TreeSetObject
+{
+	String Trainee;
+	String Course;
+	int Fees;
+	public TreeSetObject(String Trainee,String Course,int Fees)
+	{
+	this.Trainee = Trainee;
+	this.Course = Course;
+	this.Fees = Fees;
+	}
+public String toString()
+{
+return this.Trainee+" "+this.Course+" "+this.Fees;
+}
+public static void main(String[] args) 
+{
+	
+}
+
+}
