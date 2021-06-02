88af70284f3fe31a87eb56996775e54efc36cdbc
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-06-02 21:25:15 +0530

    Map

---
+++ TreeHashMapClass .java
@@ -0,0 +1,17 @@
+import java.util.TreeMap;
+
+public class TreeHashMapClass 
+{
+public static void main(String[] args) 
+{
+TreeMap t = new TreeMap();
+t.put(98, "Latha");
+t.put(96, "Vidhu");
+t.put(94, "Raja");
+t.put(89, "Vimal");
+//Ascending order of keys Maintained
+//hetrogenous keys not allowed
+System.out.println(t);
+}
+
+}
