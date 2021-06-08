c079aabf2d47ae6f35c0f99d7b366f656cd3482b
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-06-08 21:20:33 +0530

    Create ComparatorDd.java

---
+++ ComparatorDd.java
@@ -0,0 +1,28 @@
+import java.util.* ;
+public class ComparatorDd implements Comparator
+{
+public static void main(String[] args)
+{
+
+}
+@Override
+public int compare(Object o1, Object o2)
+{
+	String s1 = (String)o1;
+	String s2 = (String)o2;
+	if(s1.length()>s2.length())
+	{
+		return -1;
+	}
+	else if(s1.length()<s2.length())
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+		
+}
+
+}
