fe258b6f3a9ec1d366792e29ac52fa716edb126f
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-08-05 11:08:19 +0530

    TreeSet

---
+++ Tree.java
@@ -0,0 +1,17 @@
+import java.util.TreeSet;
+
+public class Tree 
+{
+public static void main(String[] args) 
+{
+TreeSet ts = new TreeSet();
+ts.add(50);
+ts.add(100);
+ts.add(55);
+ts.add(20);
+ts.add(2);
+//ts.add("Raja");
+System.out.println(ts);
+
+}
+}
