7a79e46ab2ce71f3825fdcdf0a1127b9b0e6f0f3
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-08-07 11:43:22 +0530

    ArrayList

---
+++ ArrayRaj.java
@@ -0,0 +1,19 @@
+package test;
+import java.util.ArrayList;
+import java.util.Collections;
+public class ArrayRaj 
+{
+public static void main(String[] args) 
+{
+ArrayList<Integer> al = new ArrayList<Integer>();
+al.add(10);
+al.add(20);
+al.add(30);
+al.add(40);
+al.add(50);
+//System.out.println("Before Reverse---->"+al);
+//Collections.reverse(al);
+//System.out.println("After Reverse----->"+al);
+
+}
+}
